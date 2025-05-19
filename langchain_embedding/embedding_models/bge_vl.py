3a47640b555dff688653693c56787bba3170fae5
yu <yul1024@outlook.com>
2025-05-19 20:07:24 +0800

    Create bge_vl.py

---
+++ langchain_embedding/embedding_models/bge_vl.py
@@ -0,0 +1,48 @@
+"""
+BGE基础的multi-modal-embedding-model。
+
+原始模型以及代码来源:
+https://huggingface.co/BAAI/BGE-VL-large
+"""
+
+from langchain_core.embeddings import Embeddings
+
+from transformers import AutoModel
+import torch
+from PIL import Image
+
+
+class BGEVL(Embeddings):
+    """
+    BGE基础的multi-modal-embedding-model。
+    """
+    def __init__(
+        self,
+        model_path: str,
+    ):
+        self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
+        self.model.set_processor(model_path)
+        self.model.eval()
+
+    def embed_documents(self, texts: list[str]) -> list[list[float]]:
+        return self.embed_text(texts)
+
+    def embed_query(self, query: str) -> list[float]:
+        return self.embed_text([query])[0]
+
+    def embed_text(
+        self,
+        inputs: list[str]
+    ) -> list[list[float]]:
+        with torch.no_grad():
+            text_features = self.model.encode(text=inputs)
+        return text_features.tolist()
+
+    def embed_image(
+        self,
+        uris: list[str],
+    ) -> list[list[float]]:
+        with torch.no_grad():
+            image_features = self.model.encode(images=uris)
+        return image_features.tolist()
+
