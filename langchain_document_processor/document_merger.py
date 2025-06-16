955e31ed551416208d3f6481338bb9993c3c711d
yu <yul1024@outlook.com>
2025-06-16 14:20:49 +0800

    Update document_merger.py

--- langchain_document_processor/document_merger.py
+++ langchain_document_processor/document_merger.py
@@ -15,16 +15,28 @@ if TYPE_CHECKING:
 
 
 class DocumentMerger:
+    # ====主要方法。====
     @staticmethod
     def get_human_message(
         query: str,
         text_documents: list[Document] | None = None,
         image_documents: list[Document] | None = None,
     ) -> HumanMessage:
-        ...
+        if text_documents is not None and image_documents is None:
+            # 仅文本文档的情况。
+            ...
+        elif text_documents is None and image_documents is not None:
+            # 仅图片文档的情况。
+            ...
+        elif text_documents is not None and image_documents is not None:
+            # 文本和图片模态都有的情况。
+            ...
+        else:
+            raise
 
     @staticmethod
     def merge_multimodal_documents(
+        query: str,
         text_documents: list[Document] | None = None,
         image_documents: list[Document] | None = None,
     ) -> list[Document]:
@@ -34,12 +46,24 @@ class DocumentMerger:
     def merge_text_documents(
         text_documents: list[Document],
     ) -> str:
-        ...
+        text_content = ""
+        for text_document in text_documents:
+            document_text = ContentAnnotator.annotate_with_xml(
+                tag='document',
+                original_text=text_document.page_content
+            )
+            document_text += '\n'
+            text_content += document_text
+        return text_content
 
     @staticmethod
     def merge_image_documents(
         image_documents: list[Document],
     ) -> list[dict]:
-        ...
+        image_content_blocks = [
+            ContentBlockProcessor.get_image_content_block_from_base64(base64_str=image_document.page_content)
+            for image_document in image_documents
+        ]
+        return image_content_blocks
 
 
