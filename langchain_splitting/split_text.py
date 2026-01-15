7715290ccbad47873858e843634d36d5c599bec9
yu <yul1024@outlook.com>
2026-01-15 19:46:41 +0800

    feat: add method to split normal text

---
+++ langchain_splitting/split_text.py
@@ -0,0 +1,19 @@
+"""
+Sources:
+    https://github.com/yuliu625/Yu-RAG-Toolkit/langchain_splitting/split_text.py
+
+References:
+
+
+Synopsis:
+    分割一般的text。
+
+Notes:
+
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
