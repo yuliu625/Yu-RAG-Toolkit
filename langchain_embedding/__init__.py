75ba3fab005f3af2062f768659b779526f5d1ee9
yu <yul1024@outlook.com>
2026-01-15 20:44:47 +0800

    docs: update docstring

--- langchain_embedding/__init__.py
+++ langchain_embedding/__init__.py
@@ -1,4 +1,9 @@
 """
-在langchain中可以使用的embedding-model。
+Synopsis:
+    langchain中可以使用的embedding-model。
+
+Notes:
+    在这个package中，重要的实现是: 对于具体的embedding model的Embedding的定义。
+        因此，主要方法基于embedding_models的sub-package中。
 """
 
