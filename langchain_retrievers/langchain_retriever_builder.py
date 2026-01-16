eb062ad6178cd6a56420d2f823ccabc172ad894c
yu <yul1024@outlook.com>
2026-01-16 20:43:45 +0800

    feat: add methods to build retrievers in langchain

---
+++ langchain_retrievers/langchain_retriever_builder.py
@@ -0,0 +1,25 @@
+"""
+Sources:
+
+References:
+
+Synopsis:
+
+Notes:
+
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class LangchainRetrieverBuilder:
+    @staticmethod
+    def build_langchain_retriever(
+
+    ):
+        ...
+
