67513874c2c93e18a20f89bbf41714df21e37ab9
yu <yul1024@outlook.com>
2026-01-16 20:56:44 +0800

    docs: update docstring

--- langchain_splitting/split_text.py
+++ langchain_splitting/split_text.py
@@ -3,7 +3,7 @@ Sources:
     https://github.com/yuliu625/Yu-RAG-Toolkit/langchain_splitting/split_text.py
 
 References:
-
+    https://docs.langchain.com/oss/python/integrations/splitters
 
 Synopsis:
     分割一般的text。
@@ -19,3 +19,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+
