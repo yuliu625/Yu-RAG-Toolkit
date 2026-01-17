7c91e473cb8b943c0f6bfd7e418776df18d6e9a6
yu <yul1024@outlook.com>
2026-01-18 01:01:27 +0800

    test: add tests

--- tests/unit_tests/test_langchain_storing/test_chroma_vector_store_builder.py
+++ tests/unit_tests/test_langchain_storing/test_chroma_vector_store_builder.py
@@ -1,11 +1,25 @@
 """
-
+测试构建Chroma vector_store的方法
 """
 
 from __future__ import annotations
 import pytest
 from loguru import logger
 
+from langchain_storing.chroma_vector_store_builder import ChromaVectorStoreBuilder
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class TestChromaVectorStoreBuilder:
+    @pytest.mark.parametrize()
+    def test_load_vector_store(
+        self,
+        persist_directory: str
+    ):
+        vector_store = ChromaVectorStoreBuilder.load_vector_store(
+            persist_directory=persist_directory,
+        )
+
 
