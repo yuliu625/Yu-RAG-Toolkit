a920a316e240b4abb716868a879cc5166b3b9618
yu <yul1024@outlook.com>
2026-01-17 19:43:12 +0800

    feat: add methods to build retrievers in langgraph

---
+++ langgraph_retrievers/retrievers/simple_retriever.py
@@ -0,0 +1,34 @@
+"""
+Sources:
+
+References:
+
+Synopsis:
+
+Notes:
+
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from langchain_core.runnables import RunnableConfig
+    from langchain_core.vectorstores import VectorStore
+
+
+class SimpleRetriever:
+    def __init__(
+        self,
+        vector_store: VectorStore,
+    ):
+        ...
+
+    async def process_state(
+        self,
+        state,
+        config: RunnableConfig,
+    ) -> dict:
+        ...
+
