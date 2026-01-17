7092fa391fad003e9459df933f110da6d2570cc0
yu <yul1024@outlook.com>
2026-01-17 18:12:10 +0800

    feat: add methods to build retrievers in langchain

--- langchain_retrievers/__init__.py
+++ langchain_retrievers/__init__.py
@@ -1,4 +1,11 @@
 """
 基于langchain定义的retrievers。
+
+Notes:
+    在langchain-v1.0后，retriever相关的内容被迁移至langchain_classic中。
+    这个标志在我看来意味者:
+        - langchain_classic中的方法应该被放弃。
+        - 复杂retriever应该以langgraph来实现。
+    因此，该包仅作为参考。更好的实现，参考langgraph_retrievers。
 """
 
