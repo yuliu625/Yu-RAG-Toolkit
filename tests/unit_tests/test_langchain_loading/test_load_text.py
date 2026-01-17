92c77276e75fdec04d7acdc267f1147afbfb8b9c
yu <yul1024@outlook.com>
2026-01-18 01:01:32 +0800

    test: add tests

--- tests/unit_tests/test_langchain_loading/test_load_text.py
+++ tests/unit_tests/test_langchain_loading/test_load_text.py
@@ -1,11 +1,32 @@
 """
-
+测试加载文本的方法。
 """
 
 from __future__ import annotations
 import pytest
 from loguru import logger
 
+from langchain_loading.load_text import TextLoadingMethods
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class TestTextLoadingMethods:
+    @pytest.mark.parametrize(
+        'text_path', [
+        (r""),
+    ])
+    def test_load_text(
+        self,
+        text_path: str,
+    ):
+        # 该方法常见约定指定的参数。
+        encoding: str = 'utf-8'
+        is_autodetect_encoding: bool = False
+        document = TextLoadingMethods.load_text(
+            text_path=text_path,
+            encoding=encoding,
+            is_autodetect_encoding=is_autodetect_encoding,
+        )
 
