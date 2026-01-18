c1d4689829d5d49941f6157d8b5c588acd503b5d
yu <yul1024@outlook.com>
2026-01-18 19:01:59 +0800

    test: add tests

--- tests/unit_tests/test_langchain_embedding/test_embedding_model_factory.py
+++ tests/unit_tests/test_langchain_embedding/test_embedding_model_factory.py
@@ -3,8 +3,39 @@
 """
 
 from __future__ import annotations
+import pytest
 from loguru import logger
 
+from langchain_embedding.embedding_model_factory import EmbeddingModelFactory
+
 from typing import TYPE_CHECKING
-# if TYPE_CHECKING:
+if TYPE_CHECKING:
+    from langchain_core.embeddings import Embeddings
+
+
+class TestEmbeddingModelFactory:
+    @pytest.mark.parametrize(
+        "model_name, repeat_penalty, temperature, stop_tokens, top_k, top_p", [
+        ('nomic-embed-text', None, None, None, None, None),
+    ])
+    def test_create_ollama_embedding_model(
+        self,
+        model_name: str,
+        repeat_penalty: float | None,
+        temperature: float | None,
+        stop_tokens: list[str] | None,
+        top_k: int | None,
+        top_p: float | None,
+    ):
+        embedding_model = EmbeddingModelFactory.create_ollama_embedding_model(
+            model_name=model_name,
+            repeat_penalty=repeat_penalty,
+            temperature=temperature,
+            stop_tokens=stop_tokens,
+            top_k=top_k,
+            top_p=top_p,
+        )
+        text_embedding = embedding_model.embed_query("Some text.")
+        logger.info(f"\nText Embedding Dim: {len(text_embedding)}")
+        logger.info(f"\nText embedding: \n{text_embedding}")
 
