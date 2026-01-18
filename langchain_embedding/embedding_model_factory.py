8bd3194883725ff8f53e885dc39e74cd5b90f1a0
yu <yul1024@outlook.com>
2026-01-18 15:11:04 +0800

    feat: add methods to build embedding model

--- langchain_embedding/embedding_model_factory.py
+++ langchain_embedding/embedding_model_factory.py
@@ -31,16 +31,51 @@ class EmbeddingModelFactory:
     def create_ollama_embedding_model(
 
     ) -> Embeddings:
-        raise NotImplementedError
+        embedding_model = OllamaEmbeddings(
+
+        )
+        return embedding_model
 
     @staticmethod
     def create_huggingface_embedding_model(
         model_name_or_path: str,
+        model_kwargs: dict,
+        encode_kwargs: dict,
+        query_encode_kwargs: dict,
+        is_multi_process: bool,
+        cache_folder: str | None,
+        is_show_progress: bool,
     ) -> Embeddings:
+        """
+        构造基于HuggingFaceEmbeddings的embedding-model。
+
+        Notes:
+            - HuggingFaceEmbeddings支持多推理后端，但约定最好使用sentence-transformers。
+            - 社区实现基于BaseModel限制，因此我在该方法中显式指定了所有参数。
+                如有需要，查看HuggingFaceEmbeddings源代码。
+
+        Args:
+            model_name_or_path:
+            model_kwargs:
+            encode_kwargs:
+            query_encode_kwargs:
+            is_multi_process (bool): 是否将encode方法在多张GPU上运行。
+            cache_folder:
+            is_show_progress (bool): 是否显示展示编码进度的progress bar。
+
+        Returns:
+            Embeddings:
+        """
         embedding_model = HuggingFaceEmbeddings(
             model_name=model_name_or_path,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs,
+            query_encode_kwargs=query_encode_kwargs,
+            multi_process=is_multi_process,
+            cache_folder=cache_folder,
+            is_show_progress=is_show_progress,
         )
-        raise NotImplementedError
+        return embedding_model
 
 
 class CachedEmbeddingModelFactory:
