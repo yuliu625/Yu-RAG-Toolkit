2a474a4c2d4e109b7e7449a07984be56f0ab7e65
yu <yul1024@outlook.com>
2026-01-21 14:41:10 +0800

    feat: add new retriever

--- langgraph_retrievers/retrievers/multi_query_retriever.py
+++ langgraph_retrievers/retrievers/multi_query_retriever.py
@@ -4,6 +4,7 @@ Sources:
 References:
 
 Synopsis:
+    基于langgraph自实现的multi_query_retriever。
 
 Notes:
 
@@ -16,19 +17,36 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from langchain_core.runnables import RunnableConfig
     from langchain_core.vectorstores import VectorStore
+    from langchain_core.documents import Document
+    from langchain_core.language_models import BaseChatModel
+    from pydantic import BaseModel
 
 
 class MultiQueryRetriever:
     def __init__(
         self,
         vector_store: VectorStore,
+        structured_llm: BaseChatModel,
     ):
-        ...
+        self._vector_store = vector_store
+        self._structured_llm = structured_llm
 
     async def process_state(
         self,
         state,
         config: RunnableConfig,
     ) -> dict:
+        raise NotImplementedError
+
+    def search_document(
+        self,
+        query: str,
+    ) -> list[Document]:
+        ...
+
+    async def rewrite_query(
+        self,
+        original_query: str,
+    ) -> list[str]:
         ...
 
