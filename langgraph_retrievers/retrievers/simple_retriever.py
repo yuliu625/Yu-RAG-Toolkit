6c22fe09274a29b88c008de43f6bd021fe2530b6
yu <yul1024@outlook.com>
2026-01-21 14:41:05 +0800

    feat: add new retriever

--- langgraph_retrievers/retrievers/simple_retriever.py
+++ langgraph_retrievers/retrievers/simple_retriever.py
@@ -16,6 +16,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from langchain_core.runnables import RunnableConfig
     from langchain_core.vectorstores import VectorStore
+    from langchain_core.documents import Document
 
 
 class SimpleRetriever:
@@ -23,12 +24,18 @@ class SimpleRetriever:
         self,
         vector_store: VectorStore,
     ):
-        ...
+        self._vector_store = vector_store
 
     async def process_state(
         self,
         state,
         config: RunnableConfig,
     ) -> dict:
+        raise NotImplementedError
+
+    def search_documents(
+        self,
+        query: str,
+    ) -> list[Document]:
         ...
 
