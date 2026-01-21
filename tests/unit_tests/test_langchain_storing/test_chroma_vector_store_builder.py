d40248ca662ad5f398cc95288c5a4d4ff045211b
yu <yul1024@outlook.com>
2026-01-21 14:40:02 +0800

    test: add tests

--- tests/unit_tests/test_langchain_storing/test_chroma_vector_store_builder.py
+++ tests/unit_tests/test_langchain_storing/test_chroma_vector_store_builder.py
@@ -54,10 +54,59 @@ class TestChromaVectorStoreBuilder:
             persist_directory=persist_directory,
             embedding_function=embedding_function,
         )
-        # vector_store.search()
-        # vector_store.similarity_search()
-        # vector_store.similarity_search_with_relevance_scores()
-        # vector_store.similarity_search_with_score()
+        logger.success(f"\nVector store loaded: \n{vector_store}\n")
+
+    @pytest.mark.parametrize(
+        "persist_directory, embedding_function", [
+        (r"D:\dataset\smart\tests\t_vector_store",
+         EmbeddingModelFactory.create_ollama_embedding_model('nomic-embed-text', None, None, None, None, None,)),
+    ])
+    def test_vector_store_methods(
+        self,
+        persist_directory: str,
+        embedding_function: Embeddings,
+    ):
+        vector_store = ChromaVectorStoreBuilder.load_vector_store(
+            persist_directory=persist_directory,
+            embedding_function=embedding_function,
+        )
+        # search
+        results_1 = vector_store.search(
+            query="haha",
+            search_type='similarity',
+        )
+        logger.debug(f"\nResults 1: \n{results_1}\n")
+        results_2 = vector_store.search(
+            query="haha",
+            search_type='mmr',
+        )
+        logger.debug(f"\nResults 2: \n{results_2}\n")
+        # similarity_search
+        results_3 = vector_store.similarity_search(
+            query="haha",
+            k=3,
+        )
+        logger.debug(f"\nResults 3: \n{results_3}\n")
+        # similarity_search_with_relevance_scores
+        results_4 = vector_store.similarity_search_with_relevance_scores(
+            query="haha",
+            k=3,
+        )
+        logger.debug(f"\nResults 4: {results_4}\n")
+        # mmr
+        results_5 = vector_store.max_marginal_relevance_search(
+            query="haha",
+            k=3,
+        )
+        logger.debug(f"\nResults 5: {results_5}\n")
+        # vector_store.max_marginal_relevance_search_by_vector()
+        # similarity_search_with_score
+        ## bad methods. not include any args
+        results_6 = vector_store.similarity_search_with_score(
+            query="haha",
+            k=3,
+        )
+        logger.debug(f"\nResults 6: {results_6}\n")
 
     @pytest.mark.parametrize(
         "persist_directory, embedding_function, documents", [
@@ -79,6 +128,7 @@ class TestChromaVectorStoreBuilder:
             embedding_function=embedding_function,
             documents=documents,
         )
+        logger.info(f"\nVector store built: \n{vector_store}\n")
 
     # @pytest.mark.parametrize()
     def test_build_new_vector_store_via_specific_method(
