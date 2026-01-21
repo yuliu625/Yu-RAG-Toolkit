575a513ea370a44b7c2155db8c1bb9d68388ed95
yu <yul1024@outlook.com>
2026-01-21 15:16:06 +0800

    feat: add new retriever

--- langgraph_retrievers/retrievers/simple_retriever.py
+++ langgraph_retrievers/retrievers/simple_retriever.py
@@ -4,6 +4,7 @@ Sources:
 References:
 
 Synopsis:
+    基于langgraph实现的基础retriever。
 
 Notes:
 
@@ -23,9 +24,12 @@ class SimpleRetriever:
     def __init__(
         self,
         vector_store: VectorStore,
+        search_configs: dict,
     ):
         self._vector_store = vector_store
+        self._search_configs = search_configs
 
+    # ==== 暴露方法。 ====
     async def process_state(
         self,
         state,
@@ -33,9 +37,31 @@ class SimpleRetriever:
     ) -> dict:
         raise NotImplementedError
 
-    def search_documents(
+    # ==== 主要方法。 ====
+    async def search_documents_by_similarity(
         self,
         query: str,
+        search_configs: dict,
     ) -> list[Document]:
-        ...
+        result_documents = await self._vector_store.asimilarity_search(
+            query=query,
+            **search_configs,
+        )
+        logger.debug(f"Found {len(result_documents)} documents")
+        logger.trace(f"Result Documents: \n{result_documents}\n")
+        return result_documents
+
+    # ==== 主要方法。 ====
+    async def search_documents_by_mmr(
+        self,
+        query: str,
+        search_configs: dict,
+    ) -> list[Document]:
+        result_documents = await self._vector_store.amax_marginal_relevance_search(
+            query=query,
+            **search_configs,
+        )
+        logger.debug(f"Found {len(result_documents)} documents")
+        logger.trace(f"Result Documents: \n{result_documents}\n")
+        return result_documents
 
