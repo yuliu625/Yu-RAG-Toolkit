c4100b69467119082f83337258efdef5a7287fe8
yu <yul1024@outlook.com>
2026-01-25 18:23:22 +0800

    test: add tests

---
+++ tests/unit_tests/test_specific_huggingface_embedding_models/test_jina_embedding_models.py
@@ -0,0 +1,12 @@
+"""
+jinnai
+"""
+
+from __future__ import annotations
+import pytest
+from loguru import logger
+
+from langchain_embedding.embedding_model_factory import EmbeddingModelFactory
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
