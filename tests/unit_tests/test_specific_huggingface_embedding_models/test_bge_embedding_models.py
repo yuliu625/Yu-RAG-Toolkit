fb4841211c11a63b1e4f516aaf0b7f3e445a5ba1
yu <yul1024@outlook.com>
2026-01-25 18:23:18 +0800

    test: add tests

---
+++ tests/unit_tests/test_specific_huggingface_embedding_models/test_bge_embedding_models.py
@@ -0,0 +1,42 @@
+"""
+BAAI
+"""
+
+from __future__ import annotations
+import pytest
+from loguru import logger
+
+from langchain_embedding.embedding_model_factory import EmbeddingModelFactory
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class TestBgeEmbeddingModels:
+    @pytest.mark.parametrize(
+        "model_name_or_path, model_kwargs, encode_kwargs, query_encode_kwargs", [
+        (r"D:\model\BAAI\bge-m3",
+         dict(trust_remote_code=True,),
+         dict(),
+         dict(),),
+    ])
+    def test_bge_m3(
+        self,
+        model_name_or_path: str,
+        model_kwargs: dict,
+        encode_kwargs: dict,
+        query_encode_kwargs: dict,
+    ):
+        embedding_model = EmbeddingModelFactory.create_huggingface_embedding_model(
+            model_name_or_path=model_name_or_path,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs,
+            query_encode_kwargs=query_encode_kwargs,
+            is_multi_process=False,
+            cache_folder=None,
+            is_show_progress=True,
+        )
+        logger.info(embedding_model)
+        logger.info(f"model_max_length: {embedding_model._client.tokenizer.model_max_length}")
+        logger.info(f"max_seq_length: {embedding_model._client.max_seq_length}")
+
