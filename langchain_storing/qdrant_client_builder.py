73e7fbb742cbd5415a91c76be1c144d69e35d028
yu <yul1024@outlook.com>
2026-02-22 20:41:47 +0800

    feat: add qdrant client builder

---
+++ langchain_storing/qdrant_client_builder.py
@@ -0,0 +1,24 @@
+"""
+Sources:
+    https://github.com/yuliu625/Yu-RAG-Toolkit/langchain_storing/qdrant_client_builder.py
+
+References:
+    https://qdrant.tech/
+
+Synopsis:
+    直接对于 qdrant client 的方法。
+
+Notes:
+    langchain 中对于 qdrant 操作有限制，直接对 client 的操作方法。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class QdrantClientBuilder:
+    ...
+
