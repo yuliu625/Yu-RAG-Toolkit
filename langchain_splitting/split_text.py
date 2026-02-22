b7cd497116c77a4d38f5099141078aaacd60046a
yu <yul1024@outlook.com>
2026-02-22 18:13:07 +0800

    feat: add methods to split documents

--- langchain_splitting/split_text.py
+++ langchain_splitting/split_text.py
@@ -6,10 +6,14 @@ References:
     https://docs.langchain.com/oss/python/integrations/splitters
 
 Synopsis:
-    分割一般的text。
+    分割一般文本的方法。
 
 Notes:
+    基于 TextSplitter ， 由正则方法实现。
 
+    约定使用场景:
+        - 简单处理: 直接构造 chunks 。
+        - 二次处理: 在高级特征处理后，为满足 length 限制，再次进行处理。
 """
 
 from __future__ import annotations
@@ -20,12 +24,27 @@ from langchain_text_splitters import (
     RecursiveCharacterTextSplitter,
 )
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence
 if TYPE_CHECKING:
     from langchain_core.documents import Document
 
 
 class TextSplittingMethods:
+    @staticmethod
+    def split_documents_recursively(
+        documents: Sequence[Document],
+        chunk_size: int,
+        chunk_overlap: int,
+    ) -> list[Document]:
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+        )
+        documents = splitter.split_documents(
+            documents=documents,
+        )
+        return documents
+
     @staticmethod
     def split_text_recursively(
         document: Document,
