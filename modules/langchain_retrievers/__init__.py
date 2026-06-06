990575a5062ef09c71e604001ce345d84b70c0c1
yu <yul1024@outlook.com>
2026-06-06 15:38:47 +0800

    refactor: move

---
+++ modules/langchain_retrievers/__init__.py
@@ -0,0 +1,11 @@
+"""
+基于 langchain 定义的 retrievers 。
+
+Notes:
+    在 langchain-v1.0 后，retriever 相关的内容被迁移至 langchain_classic 中。
+    这个标志在我看来意味者:
+        - langchain_classic 中的方法应该被放弃。
+        - 复杂 retriever 应该以 langgraph 来实现。
+    因此，该包仅作为参考。更好的实现，参考 langgraph_retrievers 。
+"""
+
