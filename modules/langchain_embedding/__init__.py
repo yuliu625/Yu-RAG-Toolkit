e4e83922d63a7532e1c3fb5137d81e92401b5cc8
yu <yul1024@outlook.com>
2026-06-06 15:38:39 +0800

    refactor: move

---
+++ modules/langchain_embedding/__init__.py
@@ -0,0 +1,9 @@
+"""
+Synopsis:
+    langchain 中可以使用的 embedding-model 。
+
+Notes:
+    在这个 package 中，重要的实现是: 对于具体的 embedding model 的 Embedding 的定义。
+        因此，主要方法基于 embedding_models 的 sub-package 中。
+"""
+
