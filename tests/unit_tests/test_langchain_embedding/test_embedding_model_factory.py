7a46860f9dfcaf6e8d008f780741cc13293ce0f4
yu <yul1024@outlook.com>
2026-06-06 16:01:17 +0800

    test: add tests

--- tests/unit_tests/test_langchain_embedding/test_embedding_model_factory.py
+++ tests/unit_tests/test_langchain_embedding/test_embedding_model_factory.py
@@ -6,11 +6,11 @@ from __future__ import annotations
 import pytest
 from loguru import logger
 
-from langchain_embedding.embedding_model_factory import EmbeddingModelFactory
+from modules.langchain_embedding.embedding_model_factory import EmbeddingModelFactory
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-    from langchain_core.embeddings import Embeddings
+    pass
 
 
 class TestEmbeddingModelFactory:
