d93b617157f3aaa5a48f690f7301b9f73c06ee67
yu <yul1024@outlook.com>
2026-06-06 16:01:35 +0800

    test: add tests

--- tests/unit_tests/test_specific_huggingface_embedding_models/test_nomic_embedding_models.py
+++ tests/unit_tests/test_specific_huggingface_embedding_models/test_nomic_embedding_models.py
@@ -6,9 +6,9 @@ from __future__ import annotations
 import pytest
 from loguru import logger
 
-from langchain_embedding.embedding_model_factory import EmbeddingModelFactory
+from modules.langchain_embedding.embedding_model_factory import EmbeddingModelFactory
 
-from typing import TYPE_CHECKING
+
 # if TYPE_CHECKING:
 
 
