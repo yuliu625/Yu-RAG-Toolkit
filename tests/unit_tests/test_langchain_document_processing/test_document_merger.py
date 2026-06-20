3444d03c05a1332d044dc19637e040ed20c169ca
yu <yul1024@outlook.com>
2026-06-20 11:56:25 +0800

    test: add tests

--- tests/unit_tests/test_langchain_document_processing/test_document_merger.py
+++ tests/unit_tests/test_langchain_document_processing/test_document_merger.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 import pytest
 from loguru import logger
 
-from modules.langchain_document_processing.document_merger import DocumentMerger
+from src.langchain_document_processing.document_merger import DocumentMerger
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
